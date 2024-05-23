03b8f73a446c4e4cc1221b0e720baa329548b8b1
SERGIO MANOEL ALVES PAES LOPES <SMAPLOPES@seds.com.br>
2024-05-23 15:45:23 -0300

    clone 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "";
+?>
+<style>
+    body{
+    background-color: #fff;
+    } 
+    .sergio{
+    } 
+</style>
+<html>
+    <title>init-prj-public</title>
+<head></head>
+<body>
+    <h1 class="sergio"></h1>
+</body>
+</html>
+<script>
+    // alert();
+</script>
+
